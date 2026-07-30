566ed93d4131ab9154c53289a40202bef3f17a83
Matvii Dzhyhyr <dzhigirmatviy@gmail.com>
2026-07-30 18:38:39 +0200

    Add example test report, clean up README

--- playwright.config.js
+++ playwright.config.js
@@ -1,13 +1,16 @@
 import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
-  reporter: 'html',
+  reporter: [
+    ['html'],
+    ['json', { outputFile: 'test-results/results.json' }],
+  ],
   use: {
     baseURL: 'http://localhost:8000',
   },
   webServer: {
-  command: 'uvicorn mock_server.app:app --port 8000',
-  port: 8000,
-  reuseExistingServer: !process.env.CI,
-},
+    command: 'uvicorn mock_server.app:app --port 8000',
+    port: 8000,
+    reuseExistingServer: !process.env.CI,
+  },
 });
